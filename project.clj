5c22171afd35d97a4748da55724549d59ebddfc7
Dennis Schridde <devurandom@gmx.net>
2018-10-03 11:30:15 +0200

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/devurandom/elog"
   :license {:name "GNU General Public License v3.0 or later"
             :url "https://www.gnu.org/licenses/gpl-3.0-standalone.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.json "0.2.6"]
                  [environ "1.1.0"]
-                 [irresponsible/tentacles "0.6.1"]
-                 [clj-time "0.14.0"]
-                 [cljstache "2.0.0"]]
+                 [irresponsible/tentacles "0.6.2"]
+                 [clj-time "0.14.4"]
+                 [cljstache "2.0.1"]]
   :main ^:skip-aot elog.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
